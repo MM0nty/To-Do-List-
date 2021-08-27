c27f369db6134f3e64240e6c0d9f88af10e5e8be
M <79332342+MM0nty@users.noreply.github.com>
2021-08-27 21:30:46 +0000

    Refactoring into object oriented programming

--- Index.js
+++ Index.js
@@ -1,21 +1,18 @@
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
     complete: false,
     state: function () {
-      console.log(`${task.title} has${task.complete ? " " : " not "} been completed`);
+      console.log(`${this.title} has${this.complete ? " " : " not "} been completed`);
     },
-    complete: function () {
-      true;
+    completed: function () {
+      this.complete = true;
     }
   };
   return task;
 }
 
-// DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the  💩  out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
@@ -23,28 +20,5 @@ const tasks = [task1, task2];
 
 
 task1.state();
-task1.complete();
+task1.completed();
 task1.state();
-
-
-/* ORIGINAL CODE (MESSY/INEFFICIENT
-
-function newTask(title, description) {
-  const task = {
-    title: title,
-    description: description,
-    complete: false
-  };
-  return task;
-}
-
-// DRIVER CODE BELOW
-
-const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
-const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
-
-// for now, let's just make sure we see our tasks
-console.log(tasks);
-
-*/
